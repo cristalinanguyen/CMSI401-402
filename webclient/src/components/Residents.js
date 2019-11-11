8278fd0342dbf2257a02151e8a18fdbadaf8daca
Chris Santander <chrissantander@Chriss-MacBook-Pro.local>
2019-11-10 20:37:15 -0800

    Tables added to resident's page

--- webclient/src/components/Residents.js
+++ webclient/src/components/Residents.js
@@ -1,14 +1,143 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import Header from './Header';
+import ReactTable from "react-table";  
+import "react-table/react-table.css";  
 
 export default class Residents extends Component {
     render() {
+
+        const data = [{  
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{  
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{  
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{  
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{  
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+            },{     
+            ID: 43432523,  
+            name: 'Kevin', 
+            last: 'Rodas',
+            year: '1999'
+
+            
+ 
+        }]  
+        const columns = [{  
+            Header: 'Resident ID',  
+            accessor: 'ID'  
+            },{  
+            Header: 'Name',  
+            accessor: 'name'  
+            },{  
+            Header: 'Last Name',  
+            accessor: 'last'  
+            },{   
+            Header: 'Year',  
+            accessor: 'year'  
+        }] 
+
         return (
+
             <div>
                 <Header/>
                 <h1>Residents Page</h1>
-            </div>
+
+                <ReactTable  
+                    data={data}  
+                    columns={columns}  
+                    defaultPageSize = {20}  
+                    // pageSizeOptions = {[2,4, 6]}  
+                />  
+            </div>   
         )
     }
 }
