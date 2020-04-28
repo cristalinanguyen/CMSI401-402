72a066306d96694e9d26070e72110af3698dc59d
stprochnow64 <stprochnow64@gmail.com>
2020-04-27 17:11:14 -0700

    add back B constraint

--- server/ward_scheduler.py
+++ server/ward_scheduler.py
@@ -26,6 +26,7 @@ class Schedule:
             print ( ("Name: %s  |  Ward: %s  |  Off: %s  " % (s.first_name, s.ward, s.off) ).center(40,' ') )
             # print (s.ward)
             x = x + 1
+        sy_list[len(sy_list) - 1].ward = 'B'
 
     def make_poss_list(self, ward_list, sy_list, fy_list):
         for f in fy_list:
